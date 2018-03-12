e7a3dd8fe773cd4dbd03d076fcbd3f9e297f79eb
xtherebellion <jlaberge@paymentus.com>
2018-03-12 09:56:35 -0400

    beta version

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello Pluto!");
+module.exports = function(lang) {
+    if(lang === "es"){
+        console.log("Hola Pluto! ");
+    } else {
+        console.log("Hello Pluto! ");
+    }
+}
